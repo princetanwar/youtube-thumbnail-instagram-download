a14d181a7aba2f6afbbefc29f22d63e04efdac26
prince tanwar <prince.tanwar@digitalhive.in>
2021-01-11 21:45:56 +0530

    bug fix

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,27 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 interface Props {
   videoLink: string;
 }
 
 const Video = ({ videoLink }: Props) => {
-  const [videoBlob, setvideoBlob] = useState("");
-  useEffect(() => {
-    fetch(videoLink)
-      .then((res) => res.blob())
-      .then((blob) => setvideoBlob(URL.createObjectURL(blob)));
-  }, [videoLink]);
   return (
-    <a
-      href={videoBlob}
-      target="_blank"
-      className="btn btn-secondary mb-2 mt-2"
-      download
-      rel="noopener noreferrer"
-      style={{ display: "inherit" }}
-    >
-      Download video
-    </a>
+    <div className="justify-content-center d-flex">
+      <video width="320" height="240" controls>
+        <source src={videoLink} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
   );
 };
 
